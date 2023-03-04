7f53ca2d47e9f2a7698ef2250072fb047dac1d29
Thomas Powell <twilliampowell@gmail.com>
2023-03-03 20:28:31 -0500

    Sample program and fixed

---
+++ chapter-01/sample-program-fixed.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+// This program displays "Hello World."
+int main () {
+  cout << "Hello World.\n";
+  return 0;
+}
---
+++ chapter-01/sample-program.cpp
@@ -0,0 +1,6 @@
+#include <iostream.h>
+// This program displays "Hello World."
+int main () {
+  cout << "Hello World.\n";
+  return 0;
+}
